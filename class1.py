466ba30d1d2fac7103ac5f25b53aca1f684b4d7a
igorpieniek <pieniek97@gmail.com>
2020-03-06 17:19:51 +0100

    Add Genre class

---
+++ class1.py
@@ -0,0 +1,6 @@
+class Genre():
+    dummys = ['M1','M2','M3', 'D1','D2', 'D3', 'D4', 'D8', 'D9', 'D11', 'B1','B2','B3']
+    stands = ['90', '45','Ślimak']
+    color = ['styropian', 'czarny', 'bialy', 'ecru', 'czerwony']
+
+
