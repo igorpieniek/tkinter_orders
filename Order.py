baccd7565f9886a54f02be73b7353717a223df38
igorpieniek <pieniek97@gmail.com>
2020-03-08 17:05:25 +0100

    Adding oppurtunity of entering order - not finished

--- Order.py
+++ Order.py
@@ -5,28 +5,83 @@ class Order():
     def __init__(self,root):
         self.genre = Genre()
         self.root = root
+        self.inputFrameNum = 0
+        self.gen  = []
+        self.model = []
+        self.color = []
+        self.inputFrame = []
+        self.modelopt = []
+        self.coloropt = []
+        self.number = []
+
+        self._genreButtons = []
+        self._delateButtons = []
+        self._colorNum = 0
+
+    def addInputFrame(self):
+        self.inputFrame.append( LabelFrame(self.root, padx = 5, pady=5) )
+        self.inputFrame[self.inputFrameNum].grid(row = 1+self.inputFrameNum ,column = 1, stick = W+N)
+        self.inputFrameNum += 1
+        self._colorNum = 0
+
+
 
     def addFrames(self):
-        self.addElementsFrame = LabelFrame(root, padx = 5, pady=5)
-        self.addElementsFrame.grid(row = 0,column = 1)
-        self.addElementsFrame.pack()
+        self.addElementsFrame = LabelFrame(self.root, padx = 5, pady=5)
+        self.addElementsFrame.grid(row = 0,column = 1, stick = W+N+E)
 
-        self.inputFrame = LabelFrame(root, padx = 5, pady=5)
-        self.inputFrame.grid(row = 1,column = 1)
-        self.inputFrame.pack()
 
-        self.controlFrame = LabelFrame(root, padx = 5, pady=5)
-        self.controlFrame.grid(rowspan = 3,column = 0)
-        self.controlFrame.pack()
+        self.controlFrame = LabelFrame(self.root, padx = 5, pady=5)
+        self.controlFrame.grid(rowspan = 2,column = 0, stick = W+N+S)
  
-        self.orderListFrame = LabelFrame(root, padx = 5, pady=5)
+        self.orderListFrame = LabelFrame(self.root, padx = 5, pady=5)
         self.orderListFrame.grid(row = 2,column = 1)
-        self.orderListFrame.pack()
 
-    def addOrderClick():
+
+
+    def addDummyClick(self):
+        self.addInputFrame()
+
+        self.model.append(StringVar())
+        self.model[ len(self.model)-1 ].set(self.genre.dummys[0])
+        self.modelopt.append( OptionMenu(self.inputFrame[self.inputFrameNum -1],  self.model[ len(self.model)-1 ],*self.genre.dummys) )
+        self.modelopt[ len(self.modelopt)-1 ].configure(width = 5)
+        self.modelopt[ len(self.modelopt)-1 ].grid(padx=20, pady=0, row=0, column=0)
+
+        self._addDummyColor()
+
+        self._addNumberEntry()
+        
+        self._genreButtons.append( Button(self.inputFrame[self.inputFrameNum -1], text= "dodaj rodzaj",padx=10, pady=0, command = lambda:self._addInputLine()) )
+        self._genreButtons[len(self._genreButtons)-1].grid( row =2, column = 0)
+
+        self._delateButtons.append( Button(self.inputFrame[self.inputFrameNum -1], text= "usuń",padx=10, pady=0, command = lambda:self._delThisInputFrame()) )
+        self._delateButtons[len(self._delateButtons)-1].grid( row = 0, column = 3)
+
+    def _addInputLine(self):
+        if   self._colorNum < len(self.genre.color):
+            self._colorNum += 1
+            self._addDummyColor()
+            self._addNumberEntry()
+
+
+    def _delThisInputFrame(self):
         pass
 
-    def addGenreClick():
+    def _addDummyColor(self):
+        self.color.append(StringVar())
+        self.color[ len(self.color)-1 ].set(self.genre.color[0])
+        self.coloropt.append( OptionMenu(self.inputFrame[self.inputFrameNum -1], self.color[ len(self.color)-1 ],*self.genre.color) )
+        self.coloropt[ len(self.coloropt)-1 ].config(width = 10)
+        self.coloropt[ len(self.coloropt)-1 ].grid(padx=20, pady=0,  row= self._colorNum, column=1)
+
+
+    def _addNumberEntry(self):
+        self.number.append( Entry(self.inputFrame[self.inputFrameNum -1], width=4) )
+        self.number[len(self.number)-1].grid( row= self._colorNum , column=2)
+
+
+    def addStateClick(self):
         pass
 
     def backClick():
@@ -38,29 +93,38 @@ class Order():
     def generatePDFClick():
         pass
 
-    def addButtons(self):
+    def addMainButtons(self):
         self.buttons = []
-        self.buttons.append(Button(self.addElementsFrame, text= "Dodaj",padx=10, pady=7, command = lambda:self.addOrderClick()))
-        self.buttons[0].grid(column = 0, sticky=W+N)
+        self.buttons.append(Button(self.addElementsFrame, text= "Dodaj manekiny",padx=10, pady=7, command = lambda:self.addDummyClick()))
+        self.buttons[0].grid(row = 0,  column = 0,padx=20, pady=3, sticky=W+N)
 
-        self.buttons.append(Button(self.addElementsFrame, text= "Dodaj rodzaj",padx=10, pady=7, command = lambda:self.addGenreClick()))
-        self.buttons[1].grid(column = 1, sticky=W+N)
+        self.buttons.append(Button(self.addElementsFrame, text= "Dodaj statyw",padx=10, pady=7, command = lambda:self.addStateClick()))
+        self.buttons[1].grid(row = 0, column = 1,padx=20, pady=3, sticky=W+N)
 
-        self.buttons.append(Button(self.controlFrame, text= "Cofnij",padx=10, pady=7, command = lambda:self.backClick()))
-        self.buttons[2].grid( sticky=S+W)
+        self.buttons.append(Button(self.controlFrame, text= "Cofnij",padx=10, pady=3, command = lambda:self.backClick()))
+        self.buttons[2].grid( sticky=S)
 
-        self.buttons.append(Button(self.controlFrame, text= "Zapisz",padx=10, pady=7, command = lambda:self.saveClick()))
-        self.buttons[3].grid( sticky=S+W)
+        self.buttons.append(Button(self.controlFrame, text= "Zapisz",padx=10, pady=3, command = lambda:self.saveClick()))
+        self.buttons[3].grid( sticky=S)
 
-        self.buttons.append(Button(self.controlFrame, text= "Generuj PDF",padx=10, pady=7, command = lambda:self.generatePDFClick()))
-        self.buttons[4].grid( sticky=S+W)
+        self.buttons.append(Button(self.controlFrame, text= "Generuj PDF",padx=10, pady=3, command = lambda:self.generatePDFClick()))
+        self.buttons[4].grid( sticky=S)
+    
 
-        
-        for i in range(len(self.buttons)):
-            self.buttons[i].pack()
+
+    def addOptionLists(self):
+        pass
+
+    def genreOptAction(self, gen):
+        if gen == self.genre.gen[0]:
+            pass
+        else:
+            pass
+
 
     def process(self):
         self.addFrames()
-        self.addButtons()
+        self.addMainButtons()
+        self.addOptionLists()
 
 
--- TkinterProject1.py
+++ TkinterProject1.py
@@ -5,7 +5,7 @@ from Order import Order
 
 
 root = Tk()
-root.geometry('400x400')
+root.geometry('800x400')
 
 global manager
 manager = windowManager(0)
@@ -17,9 +17,10 @@ order = Order(root)
 
 if manager.getStatus() == 'START':
       #START WINDOW
-    status = startwin.process()
-    if status != -1:  
-        manager.setStatus(manager.options[status])
+    order.process()
+    #status = startwin.process()
+    #if status != -1:  
+     #   manager.setStatus(manager.options[status])
   
 elif manager.getStatus() == 'NEW_ORDER':
     order.process()
@@ -27,35 +28,6 @@ elif manager.getStatus() == 'NEW_ORDER':
 #elif manager.getStatus() == 2:
     #HISTORY
 
-#############################################
-#root = tk()
 
-
-
-#start = entry(root, width=50 )
-#start.pack()
-
-
-#labels = []
-#labels.append(label(root, text ="hello  world!"))
-#labels.append(label(root, text ="label2"))
-
-
-#labels[0].grid(row = 0, column = 0, padx = 20, pady = 10)
-#labels[1].grid(row = 1, column = 1)
-
-#def click():
-#    if not start.get():
-#        return
-#    else:
-#        label1 = label(root, text = start.get())
-#        label1.pack()
-#        root.destroy()
-      
-
-#buttons = []
-#buttons.append(button(root, text= "przycisk", padx=10, pady=7, command = click, fg= "blue"))
-
-#buttons[0].pack()
 
 root.mainloop()
