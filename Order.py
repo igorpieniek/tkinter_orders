26764a88270834b3e2675b281d99e325c897a5e2
igorpieniek <pieniek97@gmail.com>
2020-03-08 17:53:21 +0100

    Dummy input line class added

--- Order.py
+++ Order.py
@@ -6,23 +6,15 @@ class Order():
         self.genre = Genre()
         self.root = root
         self.inputFrameNum = 0
-        self.gen  = []
-        self.model = []
-        self.color = []
-        self.inputFrame = []
-        self.modelopt = []
-        self.coloropt = []
-        self.number = []
 
-        self._genreButtons = []
-        self._delateButtons = []
-        self._colorNum = 0
+        self.inputFrame = []
+        self.Dummys = []
+
 
     def addInputFrame(self):
         self.inputFrame.append( LabelFrame(self.root, padx = 5, pady=5) )
         self.inputFrame[self.inputFrameNum].grid(row = 1+self.inputFrameNum ,column = 1, stick = W+N)
         self.inputFrameNum += 1
-        self._colorNum = 0
 
 
 
@@ -41,44 +33,7 @@ class Order():
 
     def addDummyClick(self):
         self.addInputFrame()
-
-        self.model.append(StringVar())
-        self.model[ len(self.model)-1 ].set(self.genre.dummys[0])
-        self.modelopt.append( OptionMenu(self.inputFrame[self.inputFrameNum -1],  self.model[ len(self.model)-1 ],*self.genre.dummys) )
-        self.modelopt[ len(self.modelopt)-1 ].configure(width = 5)
-        self.modelopt[ len(self.modelopt)-1 ].grid(padx=20, pady=0, row=0, column=0)
-
-        self._addDummyColor()
-
-        self._addNumberEntry()
-        
-        self._genreButtons.append( Button(self.inputFrame[self.inputFrameNum -1], text= "dodaj rodzaj",padx=10, pady=0, command = lambda:self._addInputLine()) )
-        self._genreButtons[len(self._genreButtons)-1].grid( row =2, column = 0)
-
-        self._delateButtons.append( Button(self.inputFrame[self.inputFrameNum -1], text= "usuń",padx=10, pady=0, command = lambda:self._delThisInputFrame()) )
-        self._delateButtons[len(self._delateButtons)-1].grid( row = 0, column = 3)
-
-    def _addInputLine(self):
-        if   self._colorNum < len(self.genre.color):
-            self._colorNum += 1
-            self._addDummyColor()
-            self._addNumberEntry()
-
-
-    def _delThisInputFrame(self):
-        pass
-
-    def _addDummyColor(self):
-        self.color.append(StringVar())
-        self.color[ len(self.color)-1 ].set(self.genre.color[0])
-        self.coloropt.append( OptionMenu(self.inputFrame[self.inputFrameNum -1], self.color[ len(self.color)-1 ],*self.genre.color) )
-        self.coloropt[ len(self.coloropt)-1 ].config(width = 10)
-        self.coloropt[ len(self.coloropt)-1 ].grid(padx=20, pady=0,  row= self._colorNum, column=1)
-
-
-    def _addNumberEntry(self):
-        self.number.append( Entry(self.inputFrame[self.inputFrameNum -1], width=4) )
-        self.number[len(self.number)-1].grid( row= self._colorNum , column=2)
+        self.Dummys.append(DummyLine( self.inputFrame[self.inputFrameNum - 1]))
 
 
     def addStateClick(self):
@@ -129,13 +84,21 @@ class Order():
 
 
 class DummyLine():
+
     def __init__(self,root):
         self.root = root
         self.model = []
+        self.modelopt = []
+        self.color = []
+        self.coloropt = []
+        self.number = []
         self._genreButtons = []
         self._delateButtons = []
         self.genre = Genre()
         self._colorNum = 0
+
+
+        self.addDummyClick()
 
     def addDummyClick(self):
 
@@ -157,7 +120,7 @@ class DummyLine():
 
     def _addDummyColor(self):
         self.color.append(StringVar())
-        self.color[ len(self.color)-1 ].set(self.genre.color[0])
+        self.color[ len(self.color)-1 ].set(self.genre.color[self._colorNum])
         self.coloropt.append( OptionMenu(self.root, self.color[ len(self.color)-1 ],*self.genre.color) )
         self.coloropt[ len(self.coloropt)-1 ].config(width = 10)
         self.coloropt[ len(self.coloropt)-1 ].grid(padx=20, pady=0,  row= self._colorNum, column=1)
