9ba5e1a6a05c570af882340cedb1c1627ffdf859
igorpieniek <pieniek97@gmail.com>
2020-03-19 20:26:58 +0100

    Add all needed functions to Dummys class

--- Dummy.py
+++ Dummy.py
@@ -2,9 +2,12 @@
 from Genre import *
 
 class Dummy(object):
-    def __init__(self, model, color = None, num = None):
-        self._model = model
+    def __init__(self, model=None, color = None, num = None):
         self._genre = Genre()
+ 
+        if model: self._model = model
+
+
         if color: self._color = color
         else: self._color = []
 
@@ -32,5 +35,9 @@ class Dummy(object):
         output = []
         for a in range(len(self._color)):
             output.append([self._color[a], self._number[a]])
-
         return output
+
+    def setModel(self, model):
+        # check in color is valid
+        if not model in self._genre.dummys: error('No such color on genre list!')
+        self._model = model    
