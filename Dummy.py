eb9cb2cf354c6cfd575b7aa053e07eb618ef7e6c
igorpieniek <pieniek97@gmail.com>
2020-03-24 20:52:38 +0100

    Dummy class fixed -> tested version

--- Dummy.py
+++ Dummy.py
@@ -12,14 +12,20 @@ class Dummy(object):
         if model and color and num: 
             self.addOption(model,color,num)
 
-    def __del__(sel):
+    def __del__(self):
         del self._model,self._color, self._number
+    
+    def __str__(self):
+        if  self._model and self._color and self._number:
+           return 'Model: ' + self._model +' Color: '+ self._color+' Number: '+str(self._number)+ ' isLocekd = ' + str(self._isLocked)
+        else: return 'Empty object'
+        
 
     def addOption(self, model,color, num):
         # check in color is valid
         if self._isLocked: print('This object was used, you cant change it ! ')
-        if not model in self._genre.model: error('No such dummy model on genre list!')
-        if not color in self._genre.color: error('No such color on genre list!')
+        if not model in self._genre.dummys :error('No such dummy model on genre list!')
+        if not color in self._genre.color : error('No such color on genre list!')
 
         if num > 0: # onlyy if number is  
             self._model = model
@@ -30,8 +36,7 @@ class Dummy(object):
 
 
     def getData(self):
-        output = []
-        if not self._model  : return output
-        output.append([self._model, self._color, self._number])
+        if not self._model  : return None
+        output = [self._model, self._color, self._number]
         return output
   
